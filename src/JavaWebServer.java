a7f202aba99d0993ce74416aea2a956fcedffc93
Cole Rudnick <league@WTS10.attlocal.net>
2018-01-18 18:29:14 -0800

    coded
    yuh

--- src/JavaWebServer.java
+++ src/JavaWebServer.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -7,6 +8,7 @@ import java.net.Socket;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -35,56 +37,62 @@ public class JavaWebServer {
 
 	}
 
-    private static void HandleRequest(Socket s)
-    {
-        BufferedReader in;
-        PrintWriter out;
-        String request;
- 
-        try
-        {
-            String webServerAddress = s.getInetAddress().toString();
-            System.out.println("New Connection:" + webServerAddress);
-            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
- 
-            request = in.readLine();
-            System.out.println("--- Client request: " + request);
- 
-            out = new PrintWriter(s.getOutputStream(), true);
-            out.println("HTTP/1.0 200");
-            out.println("Content-type: text/html");
-            out.println("Server-name: myserver");
-            String response = "<html>"
-                    + "<head>"
-                    + "<title>My Web Server</title></head>"
-                    + "<h1>Change the server code so that it can read files!</h1>"
-                    + "</html>";
-            out.println("Content-length: " + response.length());
-            out.println("");
-            out.println(response);
-            out.flush();
-            out.close();
-            s.close();
-        }
-        catch (IOException e)
-        {
-            System.out.println("Failed respond to client request: " + e.getMessage());
-        }
-        finally
-        {
-            if (s != null)
-            {
-                try
-                {
-                    s.close();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return;
-    }
- 
+	private static void HandleRequest(Socket s) {
+		BufferedReader in;
+		PrintWriter out;
+		String request;
+		String fileName = "";
+		String fileType = "";
+		String htmlCode = "";
+		
+		try {
+			String webServerAddress = s.getInetAddress().toString();
+			System.out.println("New Connection:" + webServerAddress);
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			while (in.ready()) {
+				request = in.readLine();
+				System.out.println("--- Client request: " + request);
+
+				if (request.startsWith("GET /")) {
+					for (int i = 5; i < request.length() - 9; i++) {
+						fileName += request.charAt(i);
+					}
+				}
+				System.out.println(fileName);
+			}
+
+			out = new PrintWriter(s.getOutputStream(), true);
+			out.println("HTTP/1.0 200");
+			if (fileName.contains(".js")) {
+				out.println("Content-type: application/javascript");
+			} else {
+				out.println("Content-type: text/html");
+			}
+			out.println("Server-name: myserver");
+			Scanner scanner = new Scanner(new File(fileName));
+			// out.println("Content-length: " + response.length());
+			out.println("");
+			// out.println(response);
+			while (scanner.hasNextLine()) {
+				htmlCode += scanner.nextLine();
+			}
+			out.println("Content- length: " + htmlCode.length());
+			out.println(htmlCode);
+			out.flush();
+			out.close();
+			s.close();
+		} catch (IOException e) {
+			System.out.println("Failed respond to client request: " + e.getMessage());
+		} finally {
+			if (s != null) {
+				try {
+					s.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return;
+	}
+
 }
